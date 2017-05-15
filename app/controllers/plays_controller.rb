a835c26bcde8d7d610efe274e10b5cc162ce6ee9
remy taglang <remy.taglang@gmail.com>
2017-05-15 14:16:19 +0200

    v2.0 tout fonctionne mais sans la partie timer

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -16,32 +16,32 @@ class PlaysController < ApplicationController
     # end
 
     @attempt = params[:attempt]
-    @result = run_game(@attempt, @grid, start_time, end_time)
+    @grid = params[:grid]
+    @result = run_game(@attempt, @grid)
   end
 
 
     def included?(guess, grid)
-      guess.all? { |letter| guess.count(letter) <= @grid.count(letter) }
+      guess.split("").all? { |letter| guess.split("").count(letter) <= grid.count(letter) }
     end
 
-    def compute_score(attempt, time_taken)
-      (time_taken > 60.0) ? 0 : @attempt.size * (1.0 - time_taken / 60.0)
+    def compute_score(attempt)
+      score_a = 20
     end
 
-    def run_game(attempt, grid, start_time, end_time)
-      result = { time: end_time - start_time }
-
-      result[:translation] = get_translation(@attempt)
+    def run_game(attempt, grid)
+      result = {}
+      result[:translation] = get_translation(attempt)
       result[:score], result[:message] = score_and_message(
-        @attempt, result[:translation], @grid, result[:time])
+        attempt, result[:translation], grid)
 
       result
     end
 
-    def score_and_message(attempt, translation, grid, time)
-      if included?(@attempt.upcase, @grid)
+    def score_and_message(attempt, translation, grid)
+      if included?(attempt.upcase, grid)
         if translation
-          score = compute_score(attempt, time)
+          score = compute_score(attempt)
           [score, "well done"]
         else
           [0, "not an english word"]
@@ -52,7 +52,7 @@ class PlaysController < ApplicationController
     end
 
     def get_translation(word)
-      api_key = "YOUR_SYSTRAN_API_KEY"
+      api_key = "f344bd65-939f-4a5d-be99-c0e46f227389"
       begin
         response = open("https://api-platform.systran.net/translation/text/translate?source=en&target=fr&key=#{api_key}&input=#{word}")
         json = JSON.parse(response.read.to_s)
@@ -70,8 +70,7 @@ class PlaysController < ApplicationController
 
 
     def generate_grid(grid_size)
-      @grille = Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
-      @belle_grille = @grille.join(" ")
+      Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
     end
 
 
